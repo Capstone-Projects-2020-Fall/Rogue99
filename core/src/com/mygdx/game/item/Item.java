e2e6d0350fb067b3c84ad3e39b049b79e10e4359
Kirby Chan <tuk45382@temple.edu>
2020-10-13 19:51:14 -0400

    Added Heal Potion

--- core/src/com/mygdx/game/item/Item.java
+++ core/src/com/mygdx/game/item/Item.java
@@ -1,7 +1,9 @@
 package com.mygdx.game.item;
 
+import com.mygdx.game.interactable.Character;
+
 public abstract class Item{
-    private int rarity;
-    private String sprite;
-    public abstract void use();
+    protected int rarity;
+    protected String sprite;
+    public abstract void use(Character character);
 }
--- core/src/com/mygdx/game/item/Potion.java
+++ core/src/com/mygdx/game/item/Potion.java
@@ -1,8 +1,17 @@
 package com.mygdx.game.item;
 
+import com.mygdx.game.interactable.Character;
+
 public class Potion extends Item{
+    private int healAmt;
 
-    public void use() {
+    public Potion(int rarity, String sprite, int heal) {
+        super.rarity = rarity;
+        super.sprite = sprite;
 
+        healAmt = heal;
+    }
+    public void use(Character character) {
+            character.setCurrHP(Math.min( character.getMaxHP(), character.getCurrHP() + healAmt ) );
     }
 }
