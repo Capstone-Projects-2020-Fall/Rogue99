9ae5825631d3e11ef3a59afda3943a490c3d69dd
owlMaster64 <60050511+owlMaster64@users.noreply.github.com>
2020-10-15 18:42:14 -0700

    add update and render methods

--- core/src/com/mygdx/game/item/Weapon.java
+++ core/src/com/mygdx/game/item/Weapon.java
@@ -1,16 +1,33 @@
 package com.mygdx.game.item;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.interactable.Character;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Weapon extends Item {
 
-    public Weapon(int rarity, String sprite, int heal) {
-        super.rarity = rarity;
-        super.sprite = sprite;
+    public static final int SPEED = 200;
+    private static Texture tecture;
+    float x, y;
+
+    public Weapon(float x, float y) {
+        this.x = x;
+        this.y = y;
+
+        tecture = new Texture("spritesheets/weapon.PNG");
     }
 
     @Override
     public void use(Character character) {
         character.getCurrHP();
     }
+
+    public void update(float deltaTime){
+        y += SPEED * deltaTime;
+    }
+
+    public void render(SpriteBatch batch){
+        batch.draw(tecture, x, y);
+    }
 }
+
