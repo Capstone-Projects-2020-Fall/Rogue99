4157032486a411a8646a101519c657adfee8840d
Hassan <tug14184@temple.edu>
2020-10-23 12:54:31 -0400

    modified server network listner

--- core/src/com/mygdx/game/server/GameServer.java
+++ core/src/com/mygdx/game/server/GameServer.java
@@ -15,7 +15,7 @@ public class GameServer extends Server {
 
     public GameServer() {
         server = new Server();
-        serverNetworkListener = new ServerNetworkListener();
+        serverNetworkListener = new ServerNetworkListener(server);
         server.addListener(serverNetworkListener);
         try {
             server.bind(ServerPort);
@@ -29,8 +29,13 @@ public class GameServer extends Server {
     private void registerPackets(){
         Kryo kryo = server.getKryo();
 
-        //TODO Register Packets After they are built
+        //register packets
+        kryo.register(Packets.Packet000ConnectionAnswer.class);
         kryo.register(Packets.Packet001Connection.class);
+        kryo.register(Packets.Packet002Map.class);
+        kryo.register(Packets.Packet003Movement.class);
+        kryo.register(Packets.Packet004Potion.class);
+        kryo.register(Packets.Packet005Stats.class);
     }
 
     public static void main(String[] args) {
--- core/src/com/mygdx/game/server/ServerNetworkListener.java
+++ core/src/com/mygdx/game/server/ServerNetworkListener.java
@@ -2,34 +2,37 @@ package com.mygdx.game.server;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
 import com.mygdx.game.Packets;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ServerNetworkListener  extends Listener {
 
-    public ServerNetworkListener(){
+    Server server;
 
+    public ServerNetworkListener(Server server){
+        this.server = server;
     }
 
     @Override
     public void connected(Connection connection) {
-        Packets.Packet001Connection a = new Packets.Packet001Connection();
-        a.name = "This is the server";
-        connection.sendTCP(a);
-        super.connected(connection);
     }
 
     @Override
     public void disconnected(Connection connection) {
-        super.disconnected(connection);
     }
 
     @Override
     public void received(Connection connection, Object object) {
-        //TODO Deal with received Packtes after they are built
         if(object instanceof Packets.Packet001Connection){
-            System.out.println(connection.toString() + ((Packets.Packet001Connection) object).name);
+            //TODO connection handling
+            Packets.Packet000ConnectionAnswer answer = new Packets.Packet000ConnectionAnswer();
+            connection.sendTCP(answer);
+        } else {
+            server.sendToAllExceptTCP(connection.getID(), object);
         }
-        super.received(connection, object);
     }
 
 }
