9ebfa00df704ef390ecce839c1929694ad162b50
Kirby Chan <tuk45382@temple.edu>
2020-11-12 18:13:40 -0500

    Fixed Enemy getting stuck behind items and began development for ranged weapons

---
+++ core/src/com/mygdx/game/item/Bow.java
@@ -0,0 +1,50 @@
+package com.mygdx.game.item;
+
+import com.mygdx.game.interactable.Character;
+
+public class Bow extends Item{
+
+    boolean equipped;
+    private String sprite;
+    private int dmgModifier;
+
+    public Bow(int rarity, String sprite, int dmgModifier){
+        super.rarity = rarity;
+        this.sprite = sprite;
+        this.dmgModifier = dmgModifier;
+    }
+
+    @Override
+    public boolean use(Character character) {
+        return true;
+    }
+
+    @Override
+    public int getId() {
+        return Item.BOW;
+    }
+
+    @Override
+    public void setEquipped(boolean equipped) {
+        this.equipped = equipped;
+    }
+
+    public boolean isEquipped() {
+        return equipped;
+    }
+
+    @Override
+    public int getDmgModifier() {
+        return dmgModifier;
+    }
+
+    @Override
+    public String getSprite() {
+        return sprite;
+    }
+
+    /* weapons sprites
+     * s143
+     * s252 to s260
+     * */
+}
