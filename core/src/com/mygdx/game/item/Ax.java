73ddc564943bc5e803462380c147d1c1f954e2e2
owlMaster64 <60050511+owlMaster64@users.noreply.github.com>
2020-11-15 12:21:30 -0800

    added weapons

---
+++ core/src/com/mygdx/game/item/Ax.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.item;
+
+public class Ax extends Weapon{
+    public Ax(int rarity, String sprite, int dmgModifier) {
+        super(rarity, "axe", dmgModifier);
+    }
+}
---
+++ core/src/com/mygdx/game/item/sword.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.item;
+
+public class sword extends Weapon{
+    public sword(int rarity, String sprite, int dmgModifier) {
+        super(rarity, "sword", dmgModifier);
+    }
+}
